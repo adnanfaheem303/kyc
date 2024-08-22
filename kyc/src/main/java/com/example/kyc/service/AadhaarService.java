082897635134eb7289cbbf3ed0bc9b01d8b00998
Faheem.Adnan <faheem.adnan@cashe.co.in>
2024-08-22 11:46:05 +0530

    Merge remote-tracking branch 'origin/master'

--- kyc/src/main/java/com/example/kyc/service/AadhaarService.java
+++ kyc/src/main/java/com/example/kyc/service/AadhaarService.java
@@ -32,29 +32,6 @@ public class AadhaarService {
     @Autowired
     private OKYCRepository okycRepository;
 
-    // This method adds a record to the Customer table
-//    public void createCustomerRecord(AadhaarDTO aadhaarDTO) {
-//        Customer customer = new Customer();
-//        customer.setUserId(aadhaarDTO.getUserId());
-//        customer.setFullAadhaarNumber(aadhaarDTO.getAadhaarNumber());
-//        customer.setLastFourDigitsOfAadhaar(aadhaarDTO.getAadhaarNumber().substring(aadhaarDTO.getAadhaarNumber().length() - 4));
-//        customer.setCreatedDate(LocalDateTime.now());
-//        customer.setUpdatedDate(LocalDateTime.now());
-//        customer.setCreatedBy(aadhaarDTO.getUserId()); // Assuming createdBy is the user ID
-//        customer.setUpdatedBy(aadhaarDTO.getUserId()); // Assuming updatedBy is the user ID
-//        customer.setKycDedupStatus(Customer.KycDeduplicationStatus.KYC_completed); // Default status
-//        customer.setKycType(Customer.KycType.CKYC); // Default type
-//
-//        customerRepository.save(customer);
-//    }
-
-    // This method adds a record to the Aadhaar table
-//    public void createAadhaarRecord(AadhaarDTO aadhaarDTO) {
-//        com.example.kyc.entity.Aadhaar aadhaar = new com.example.kyc.entity.Aadhaar();
-//        aadhaar.setUserId(aadhaarDTO.getUserId());
-//        aadhaar.setAadhaarNumber(aadhaarDTO.getAadhaarNumber());
-//        aadhaarRepository.save(aadhaar);
-//    }
 
     // This method retrieves all records from the Aadhaar table
     public List<AadhaarDTO> getAllAadhaarRecords() {
